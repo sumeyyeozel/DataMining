4f5391294dcba2ad1eb08ffded33631b6d72f8ac
ericjkl <eric.jaekel@web.de>
2024-04-18 23:42:20 +0200

    runtime analysis and plots

--- dm/gen_sol.py
+++ dm/gen_sol.py
@@ -1,8 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from evalplus.data import get_human_eval_plus, write_jsonl
+from dm.evalplus import get_human_eval_plus, write_jsonl
 import torch
+import time
+import matplotlib.pyplot as plt
+
+start = time.time()
+start_cpu = time.process_time()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Computing device: {device}")
 
 tokenizer = AutoTokenizer.from_pretrained("NinedayWang/PolyCoder-2.7B")
 model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B")
@@ -30,8 +36,30 @@ samples = [
     for task_id, problem in get_human_eval_plus().items()
 ]
 
+
+# runtime measurements
+
+if torch.cuda.is_available(): torch.cuda.synchronize()
+end = time.time()
+end_cpu = time.process_time()
+
+clock_t = end-start
+cpu_t = end_cpu-start_cpu
+print("Wall-clock time [s]: ",clock_t)
+print("CPU time [s]: ",cpu_t)
+
+
 # Ensure each solution is a string, not a list
 for sample in samples:
     sample["solution"] = str(sample["solution"])
 
 write_jsonl("samples.jsonl", samples)
+
+
+fig, ax = plt.subplots()
+plt.bar(["CPU time", "Wall-clock time"], [cpu_t, clock_t])
+ax.set_ylabel('Time (s)')
+ax.set_title('Sample generation')
+plt.savefig('runtime.png')
+
+
